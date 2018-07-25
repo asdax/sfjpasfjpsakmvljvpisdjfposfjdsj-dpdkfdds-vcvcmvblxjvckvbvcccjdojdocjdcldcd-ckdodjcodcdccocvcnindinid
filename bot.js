ab4794e9a333ec2491134bc602278c6f833cf22d
asdax <41585615+asdax@users.noreply.github.com>
2018-07-25 22:07:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- const prefix = "$";
+ const prefix = "-";
 
 
 
@@ -93,7 +93,7 @@ client.on('ready', () => {
 
 
     const devs = ['326922071185817603' , '' , '' , ''];
-const adminprefix = "$";
+const adminprefix = "-";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
@@ -124,7 +124,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if(!message.channel.guild) return;
-if(message.content.startsWith('$bc')) {
+if(message.content.startsWith('-bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
