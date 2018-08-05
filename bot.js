03d3eb9fd69d637eef1e14d755757fcab2462232
asdax <41585615+asdax@users.noreply.github.com>
2018-08-05 15:28:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,8 @@ if(!gg) return;
 if(gg) {
 gg.send({embed : new Discord.RichEmbed()
 .setDescription(`**المده التي يدخل فيها الدسكورد : \n ${lan}\n مده دخولك للدسكورد :\n ${md} \n معه مايك ؟ :\n ${br}  **`)  
-          .setFooter(`MineCubes.`)
+.addField("المقدم", `<@${message.member.id}> with ID ${message.member.id}`)          
+.setFooter(`MineCubes.`)
 .setTimestamp()
 });
 }        
